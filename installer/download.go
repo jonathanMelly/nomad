54599d6659634860f1f587b3de4c1f98b1c3a23f
Jonathan Melly <jonathan.melly@gmail.com>
2022-11-29 14:13:28 +0100

    fix(download-403): add http headers to avoid 403

--- installer/download.go
+++ installer/download.go
@@ -10,6 +10,10 @@ import (
 // getRequestBody returns the page HTML
 func getRequestBody(url string) (string, error) {
 	r, err := http.NewRequest("GET", url, nil)
+
+	r.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
+	r.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11`)
+
 	if err != nil {
 		return "", err
 	}
