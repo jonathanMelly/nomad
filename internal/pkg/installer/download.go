cda74128a7305dbf88b1fe6f8d8db87d16036766
Jonathan Melly <jonathan.melly@gmail.com>
2023-03-14 13:35:04 +0100

    feat(download): more info on error

--- internal/pkg/installer/download.go
+++ internal/pkg/installer/download.go
@@ -1,6 +1,8 @@
 package installer
 
 import (
+	"errors"
+	"fmt"
 	"github.com/gologme/log"
 	"github.com/jonathanMelly/nomad/internal/pkg/configuration"
 	"io"
@@ -18,14 +20,14 @@ func getRequestBody(url string) (string, error) {
 
 	apiKey := configuration.Settings.GithubApiKey
 	if apiKey != "" && strings.Contains(url, "github") {
-		r.Header.Add("Authorization", "Bearer "+apiKey)
+		r.Header.Add("Authorization", fmt.Sprint("Bearer ", apiKey))
 	}
 	r.Header.Add("Accept", `text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8`)
 	r.Header.Add("User-Agent", `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11`)
 
 	client, err := http.DefaultClient.Do(r)
 	if err != nil {
-		return "", err
+		return "", errors.New(fmt.Sprintln("HTTP error:", r, "|", err))
 	}
 
 	defer func(Body io.ReadCloser) {
