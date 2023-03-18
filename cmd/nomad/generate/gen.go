478e18e0dc5f24474cffb6b5b58b6d9e68f48ae0
Jonathan Melly <jonathan.melly@gmail.com>
2023-03-18 16:20:04 +0100

    fix(build): use CET timezone

--- cmd/nomad/generate/gen.go
+++ cmd/nomad/generate/gen.go
@@ -11,8 +11,9 @@ func main() {
 	log.EnableLevelsByNumber(10)
 	cwd, _ := os.Getwd()
 	target := filepath.Join(cwd, "date.txt")
+	swissLoc, _ := time.LoadLocation("Europe/Zurich")
 
-	datetimeStr := time.Now().Format("2006-01-02 15:04:05")
+	datetimeStr := time.Now().In(swissLoc).Format("2006-01-02 15:04:05")
 	err := os.WriteFile(target, []byte(datetimeStr), os.ModePerm)
 	if err != nil {
 		log.Errorln("Cannot print", datetimeStr, "to", target, err)
