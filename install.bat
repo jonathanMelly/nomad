b0ec19bf5a46d0bd9f145830154f8a49bcb5201e
Jonathan Melly <jonathan.melly@gmail.com>
2024-02-22 14:25:29 +0100

    fix(del): use win del instead of rm

--- install.bat
+++ install.bat
@@ -3,5 +3,5 @@ echo INSTALLING NOMAD, please wait
 set source=https://github.com/jonathanMelly/nomad/releases/latest/download/nomad-latest-win64.zip
 powershell -command "Start-BitsTransfer -Source %source% -Destination nomad.zip"
 powershell -command "Expand-Archive -Force nomad.zip ."
-rm nomad.zip
+del nomad.zip
 start cmd /k nomad.exe --help
