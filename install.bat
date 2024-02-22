cbb44cbbb394a3005af7939173c52cc0f28fb551
Jonathan Melly <jonathan.melly@gmail.com>
2024-02-22 14:21:56 +0100

    feat(install): direct install

---
+++ install.bat
@@ -0,0 +1,7 @@
+@echo off
+echo INSTALLING NOMAD, please wait
+set source=https://github.com/jonathanMelly/nomad/releases/latest/download/nomad-latest-win64.zip
+powershell -command "Start-BitsTransfer -Source %source% -Destination nomad.zip"
+powershell -command "Expand-Archive -Force nomad.zip ."
+rm nomad.zip
+start cmd /k nomad.exe --help
